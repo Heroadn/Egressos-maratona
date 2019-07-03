f433bd1bb582909a938e72a5ea360c9ae7b1ed66
Thiago do Nascimento <thiago@desenvolvedor.tech>
2019-07-03 16:54:04 -0300

    Update banco_vazio.sql

---
+++ banco_vazio.sql
@@ -0,0 +1,25 @@
+create schema zadmin_hackthon;
+
+create table turma(
+	id INTEGER AUTO_INCREMENT,
+    nome VARCHAR(80) NOT NULL
+);
+
+create table curso(
+	id INTEGER AUTO_INCREMENT,
+    nome VARCHAR(80) NOT NULL
+);
+
+create table usuarios(
+	id INTEGER AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    ultimo_nome VARCHAR(50) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    senha char(64) NOT NULL,
+    pergunta VARCHAR(200) NOT NULL,
+	resposta VARCHAR(200) NOT NULL,
+    turma VARCHAR(20) NOT NULL,
+    ano INTEGER NOT NULL,
+    id_turma INTEGER,
+    id_curso INTEGER
+);
