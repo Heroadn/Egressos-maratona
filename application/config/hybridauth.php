b49500e8b9bb04d8ed8dbf8f41296aa867c0d4cf
benbenjamin554@gmail.com <benbenjamin554@gmail.com>
2019-07-18 20:15:24 -0300

    Bug fixed

--- application/config/hybridauth.php
+++ application/config/hybridauth.php
@@ -23,7 +23,7 @@ $config['hybridauth'] = array(
         ),
         "Facebook" => array(
             "enabled" => TRUE,
-            "keys" => array("id" => "944857609192270", "secret" => "39a17449113e00ce1725939e50597302"), 
+            "keys" => array("id" => "944857609192270" , "secret" => "39a17449113e00ce1725939e50597302" ), 
             "trustForwarded" => FALSE,
         ),
         "Twitter" => array(
