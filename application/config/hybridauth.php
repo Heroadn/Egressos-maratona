830c1c27cb0ffbea614aaf4b86aaa614afb8bf83
benbenjamin554@gmail.com <benbenjamin554@gmail.com>
2019-07-18 19:58:43 -0300

    bud de api

--- application/config/hybridauth.php
+++ application/config/hybridauth.php
@@ -23,7 +23,7 @@ $config['hybridauth'] = array(
         ),
         "Facebook" => array(
             "enabled" => TRUE,
-            "keys" => array("id" => "944857609192270", "secret" => "39a17449113e00ce1725939e50597302"), 
+            "keys" => array("id" => "944857609192270a", "secret" => "39a17449113e00ce1725939e50597302a"), 
             "trustForwarded" => FALSE,
         ),
         "Twitter" => array(
