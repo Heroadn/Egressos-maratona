8942b91e4558c12f23c12a5b706b83b0f6b17933
benbenjamin554@gmail.com <benbenjamin554@gmail.com>
2019-07-19 11:05:44 -0300

    Facebook add table usuario

--- banco_vazio.sql
+++ banco_vazio.sql
@@ -36,6 +36,7 @@ create table usuario(
     email VARCHAR(80) NOT NULL,
     senha char(64) NOT NULL,
     oauth VARCHAR(80),
+    Facebook VARCHAR(80),
     pergunta VARCHAR(200) NOT NULL,
 	resposta VARCHAR(200) NOT NULL,
     descricao VARCHAR(200) NOT NULL,
