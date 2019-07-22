94cde02ca60f3e29afb5d18254a99ba7515dd333
benbenjamin554@gmail.com <benbenjamin554@gmail.com>
2019-07-22 15:20:12 -0300

    fix

--- application/models/Model_postagem.php
+++ application/models/Model_postagem.php
@@ -124,6 +124,9 @@ class Model_postagem extends CI_Model
 
     public function cadastroMidia($array){
         $array['status_id_status'] = 1;
+
+        var_dump($array);
+        die();
         $this->db->insert("midia", $array);
         return $this->db->insert_id();
     }
